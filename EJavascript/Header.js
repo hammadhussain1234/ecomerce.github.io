c8e0e7cbcd028872e048892f3faf6eeb569dd88f
hammadhussain1234 <122485603+hammadhussain1234@users.noreply.github.com>
2023-08-19 13:11:54 -0700

    ecommerce

---
+++ EJavascript/Header.js
@@ -0,0 +1,149 @@
+document.getElementById("header").innerHTML+=`
+<nav class="navbar navbar-expand-lg  me-auto fixed-top bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand"><img src="" width="100px" alt=""></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+  
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active " aria-current="page" href="Home.html" style="color: #000;font-size: 20px;font-weight: bold;" >HOME</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="Men.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #000;font-size: 20px;font-weight: bold;">
+        MEN
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown" id="ha">
+          <li><a class="dropdown-item" href="MenTshert.html">T-Shirt</a></li>
+          <li><a class="dropdown-item" href="polomen.html">Polo</a></li>
+          <li><a class="dropdown-item" href="Shirt.html">Shirt</a</li>
+          <li><a class="dropdown-item" href="Short.html">Short</a></li>
+          <li><a class="dropdown-item" href="Trouser.html">Trouser|Chinos</a></li>
+          <li><a class="dropdown-item" href="Pent.html">Pants</a></li>
+          <li><a class="dropdown-item" href="Mfragrance.html">Fragrance|BodySpray</a></li>
+          <li><a class="dropdown-item" href="Boxer.html">Boxerc</a></li>
+          <li><a class="dropdown-item" href="Socks.html">Socks</a></li>
+          <li><a class="dropdown-item" href="Belts.html">Belts</a></li>
+          <li><a class="dropdown-item" href="Footwear.html">Footwear</a></li>
+          <li><a class="dropdown-item" href="Track.html">Track Suit</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #000;font-size: 20px;font-weight: bold;">
+          WOMEN
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="WomenTs.html">T-Shirt</a></li>
+          <li><a class="dropdown-item" href="wshirt.html">Shirt</a></li>
+          <li><a class="dropdown-item" href="wkurti.html">Kurtis|Fusion Top</a></li>
+          <li><a class="dropdown-item" href="Stitched.html">Stitched 2Pcs Suit</a></li>
+          <li><a class="dropdown-item" href="pajama.html">Pajamas|Trouser</a></li>
+          <li><a class="dropdown-item" href="#">Night Suit</a></li>
+          <li><a class="dropdown-item" href="#">Leggings</a></li>
+          <li><a class="dropdown-item" href="#">Plazo</a></li>
+          <li><a class="dropdown-item" href="#">Skirt</a></li>
+          <li><a class="dropdown-item" href="#">Longwear</a></li>
+          <li><a class="dropdown-item" href="#">Fragrance|BodySpray</li>
+          <li><a class="dropdown-item" href="#">HandBags</a></li>
+          <li><a class="dropdown-item" href="#">Footwear</a></li>
+          <li><a class="dropdown-item" href="#">Activewear</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #000;font-size: 20px;font-weight: bold;">
+          KIDS
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="width: max-content;" >
+          <div class="container-fliud">
+              <div class="container">
+                 <div class="row">
+              <div class="col-lg-3 col-md-3 col-sm-12">
+            <h5>INFANT SUIT<br>
+                0-3 MONTH</h5>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-12">
+                   <h6>BOYS</h6>
+                  <li><a class="dropdown-item" href="#">T-Shirt</a></li>
+                  <li><a class="dropdown-item" href="#">Polo</a></li>
+                  <li><a class="dropdown-item" href="#">Shirt</a></li>
+                  <li><a class="dropdown-item" href="#">Kurta</a></li>
+                  <li><a class="dropdown-item" href="#">Sandose</a></li>
+                  <li><a class="dropdown-item" href="#">Rompers|Bodysuit</a></li>
+                  <li><a class="dropdown-item" href="#">Shorts</a></li>
+                  <li><a class="dropdown-item" href="#">Trouser</a></li>
+                  <li><a class="dropdown-item" href="#">Boxerc</a></li>
+                  <li><a class="dropdown-item" href="#">Night Suit</a></li>
+                  <li><a class="dropdown-item" href="#">2 PC Suit</a></li>
+                  <li><a class="dropdown-item" href="#">Pant</a></li>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-12">
+                  <H6>GIRL</H6>
+                  <li><a class="dropdown-item" href="#">T-Shirt</a></li>
+                  <li><a class="dropdown-item" href="#">Sandose</a></li>
+                  <li><a class="dropdown-item" href="#">Frocks</a></li>
+                  <li><a class="dropdown-item" href="#">Pret</a></li>
+                  <li><a class="dropdown-item" href="#">Kurtis|Fusion Top</a></li>
+                  <li><a class="dropdown-item" href="#">Rompers|Bodysuit</a></li>
+                  <li><a class="dropdown-item" href="#">Short</a></li>
+                  <li><a class="dropdown-item" href="#">Plazo</a></li>
+                  <li><a class="dropdown-item" href="#">Skirt</a></li>
+                  <li><a class="dropdown-item" href="#">Panties</a></li>
+                  <li><a class="dropdown-item" href="#">Leggings|Trouser Pajamas</a></li>
+                  <li><a class="dropdown-item" href="#">Night Suit</a></li>
+                  <li><a class="dropdown-item" href="#">2 PC Suit</a></li>
+                  <li><a class="dropdown-item" href="#">Pant</a></li>
+                  <li><a class="dropdown-item" href="#"></a></li>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-12">
+                  <H6>ASSESSORIES</H6>
+                  <li><a class="dropdown-item" href="#">Glasses</a></li>
+                  <li><a class="dropdown-item" href="#">Fragrance</a></li>
+                  <li><a class="dropdown-item" href="#">Watch</a></li>
+                  <li><a class="dropdown-item" href="#">Cap</a></li>
+              </div>
+          </div>  
+              </div>
+          </div>
+         
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #000;font-size: 20px;font-weight: bold;">
+          FRAGRANCE
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">Men</a></li>
+          <li><a class="dropdown-item" href="#">Women</a></li>
+          <li><a class="dropdown-item" href="#">Atter</a></li>
+          <li><a class="dropdown-item" href="#">Body Spray</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #000;font-size: 20px;font-weight: bold;">
+        ISLAMIC CORNER
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">Janamaz</a></li>
+          <li><a class="dropdown-item" href="#">Tasbih</a></li>
+          <li><a class="dropdown-item" href="#">Bakhoor Sticks</a></li>
+          <li><a class="dropdown-item" href="#">Pray Caps</a></li>
+          <li><a class="dropdown-item" href="#">Bakhoor Burner & Powder</a></li>
+          <li><a class="dropdown-item" href="#">Herbal Oil</a></li>
+          <li><a class="dropdown-item" href="#">Miswak</a></li>
+          <li><a class="dropdown-item" href="#">Ihram</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active " aria-current="page" href="HomeTextile.html" style="color: #000;font-size: 20px;font-weight: bold;" >HOME TEXTILE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Accessories.html" style="color: #000;font-size: 20px;font-weight: bold;" >ACCESSORIES</a>
+      </li>
+  </ul>
+  </div>
+</div>
+
+</nav> `
